1ab37503d291646935860f28a05e70503a7ee50f
Aleksandr Gulbickij <4efirr@gmail.com>
2019-02-24 22:46:02 +0200

    First commit -  rough layout + base logic for date selection

---
+++ src/Components/Form/form.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import moment from 'moment'
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sellYear: "",
+      sellMonth: "",
+      sellDay: "",
+      sellPrice: "",
+      purchaseYear: "",
+      purchaseMonth: "",
+      purchaseDay: "",
+      purchasePrice: "",
+      yearRange: this.yearRange(),
+      monthRange: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
+      currentYear: new Date().getFullYear()
+    };
+    this.handleDate = this.handleDate.bind(this);
+    this.handlePrice = this.handlePrice.bind(this);
+  }
+
+  yearRange(){
+    const currentYear = new Date().getFullYear(),
+          minYear = currentYear - 90,
+          maxYear = currentYear + 11;
+    let yearRange = [];
+
+    for (  let i = minYear; i < maxYear; i++  ){
+      yearRange.push(i);
+    }
+    return yearRange;
+  }
+
+  dayRange(year, month){
+    const maxDays = moment(year+"-"+month, "YYYY-MM").daysInMonth();
+    let dayRange = [];
+    for (  let i = 1; i < maxDays; i++  ){
+      dayRange.push(i);
+    }
+    return dayRange;
+  }
+
+  handleDate(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handlePrice(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    const purchaseDays = this.dayRange(this.state.purchaseYear, this.state.purchaseMonth);
+    const sellDays = this.dayRange(this.state.sellYear, this.state.sellMonth);
+    return (
+      <form>
+        <h1>Form</h1>
+
+        <h3> Pirkimas </h3>
+        <div className="row">
+          <div className="col">
+            <h4>Data</h4>
+            <div className="form-group">
+              <select
+                onChange={this.handleDate}
+                name="purchaseYear"
+                className="form-control form-control-lg"
+                value={this.state.currentYear}>
+                {this.state.yearRange.map((e, key) => {
+                   return <option key={e} value={e} >{e}</option>;
+                 })}
+              </select>
+              <select
+                onChange={this.handleDate}
+                name="purchaseMonth"
+                className="form-control form-control-lg" >
+                <option>Pasirinkite Menesi</option>
+                {this.state.monthRange.map((e, key) => {
+                   return <option key={e} value={e}>{e}</option>;
+                 })}
+              </select>
+              <select
+                onChange={this.handleDate}
+                name="purchaseDay"
+                className="form-control form-control-lg">
+                <option>Pasirinkite Diena</option>
+                {purchaseDays.map( (e) => {
+                  return <option key={e} value={e}>{e}</option>;
+                })}
+              </select>
+            </div>
+            <h4>Kaina</h4>
+            <div className="form-group">
+              <input
+                type="number"
+                name="purchasePrice"
+                onChange={this.handlePrice}
+                className="form-control" type="text"/>
+            </div>
+          </div>
+          <div className="col">
+            <h3> pasirinkta:  </h3>
+            <ul className="list-group">
+              <li className="list-group-item">Metai {this.state.purchaseYear}</li>
+              <li className="list-group-item">Menuo {this.state.purchaseMonth}</li>
+              <li className="list-group-item">Diena {this.state.purchaseDay}</li>
+              <li className="list-group-item">Kaina {this.state.purchasePrice}</li>
+            </ul>
+          </div>
+        </div>
+
+        <h3> Pardavimas </h3>
+        <div className="row">
+          <div className="col">
+            <h3> Data </h3>
+            <div className="form-group">
+              <select
+                onChange={this.handleDate}
+                name="sellYear"
+                className="form-control form-control-lg" value={this.state.currentYear}>
+                {this.state.yearRange.map((e, key) => {
+                   return <option key={e} value={e}>{e}</option>;
+                 })}
+              </select>
+              <select
+                onChange={this.handleDate}
+                name="sellMonth"
+                className="form-control form-control-lg">
+                <option>Pasirinkite Menesi</option>
+                {this.state.monthRange.map((e, key) => {
+                   return <option key={e} value={e}>{e}</option>;
+                 })}
+              </select>
+              <select
+                onChange={this.handleDate}
+                name="purchaseDay"
+                className="form-control form-control-lg">
+                <option>Pasirinkite Diena</option>
+                {sellDays.map( (e) => {
+                  return <option key={e} value={e}>{e}</option>;
+                })}
+              </select>
+            </div>
+            <h4>Kaina</h4>
+            <div className="form-group">
+              <input
+                type="number"
+                name="sellPrice"
+                onChange={this.handlePrice}
+                className="form-control" type="text"/>
+            </div>
+          </div>
+          <div className="col">
+            <h3> pasirinkta: </h3>
+            <ul className="list-group">
+              <li className="list-group-item">Metai {this.state.sellYear}</li>
+              <li className="list-group-item">Menuo {this.state.sellMonth}</li>
+              <li className="list-group-item">Diena {this.state.sellDay}</li>
+              <li className="list-group-item">Kaina {this.state.sellPrice}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <div className="form-check">
+            <input className="form-check-input" name="option" type="radio" value="" />
+            <label>Vienintelis turtas</label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" name="option" type="radio" value="" />
+            <label>Turiu dar</label>
+          </div>
+        </div>
+        <button className="btn btn-primary">Skaiciuoti</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
