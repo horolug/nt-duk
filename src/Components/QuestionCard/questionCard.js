2f485a4dcb5cede3df81fadb1ea29929dde52513
Aleksandr Gulbickij <4efirr@gmail.com>
2019-03-16 16:34:51 +0200

    cleaning up question card component

--- src/Components/QuestionCard/questionCard.js
+++ src/Components/QuestionCard/questionCard.js
@@ -2,17 +2,6 @@ import React from 'react';
 
 class questionCard extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
-
-  displayOption(){
-    // calculations are made based on dwellingStatus and duration of the status
-    // selection to be passed to parent form
-    // q1. ar deklaruota gyvenimo vieta?
-    // q1.1 [yes] - ar daugiau nei 2 metus deklaruota ?
-  }
-
   render() {
     let questionCard2 = "";
     let selectedButton = "btn-info btn mr-2";
@@ -53,20 +42,18 @@ class questionCard extends React.Component {
       <div className="mb-2">
         <div className="card">
           <div className="card-body">
-            <div className="form-check">
-              <label className="form-check-label" htmlFor="primaryDwelling">
-                Ar parduodamas turtas - deklaruota gyvenimo vieta
-              </label>
-              <div className="mt-4">
-                <button
-                  id="isPrimaryDwelling"
-                  onClick={(e)=>this.props.onClick(e)}
-                  className={selectedButton}>Taip</button>
-                <button
-                  id="notPrimaryDwelling"
-                  onClick={(e)=>this.props.onClick(e)}
-                  className="btn btn-info">Ne</button>
-              </div>
+            <label className="form-check-label" htmlFor="primaryDwelling">
+              Ar parduodamas turtas - deklaruota gyvenimo vieta
+            </label>
+            <div className="mt-4">
+              <button
+                id="isPrimaryDwelling"
+                onClick={(e)=>this.props.onClick(e)}
+                className={selectedButton}>Taip</button>
+              <button
+                id="notPrimaryDwelling"
+                onClick={(e)=>this.props.onClick(e)}
+                className="btn btn-info">Ne</button>
             </div>
           </div>
         </div>
