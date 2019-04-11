d588db03b2c962edd0aff20b0fe3318c6bb1f321
Aleksandr Gulbickij <4efirr@gmail.com>
2019-04-11 22:57:51 +0300

    Adding disqus comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Form from './Components/Form/form'
+import Form from './Components/Form/form';
+import Disqus from 'disqus-react';
 
 
 class App extends Component {
   render() {
+    const disqusShortname = 'paskaiciuok';
+
     return (
       <div className="App">
-        <header className="mb-4">
+        <header className="mb-4 pb-2 pt-2">
           <div className="container">
             <h1 className="d-block text-center"> header </h1>
             <p className="lead text-center">Įrankis skirtas GPM skaičiavimui parduodant nekilnojamą turtą</p>
@@ -14,18 +17,23 @@ class App extends Component {
         </header>
         <div className="container">
           <Form />
+
+          <div className="border p-2 rounded-sm mt-4">
+            <Disqus.DiscussionEmbed shortname={disqusShortname} />
+
+          </div>
         </div>
         <footer className="mt-4 pt-4 pb-2">
           <div className="container">
             <div className="row">
               <div className="col">
-                <p>Visi skaičiavimai yra informacinio pobūdžio. Jeigu kyla papildomų klausimų dėl mokėsčių mokėjimo rekomenduojame kreiptis pas mokėsčių apskaitos profesionalus</p>
+                <p>Visi skaičiavimai yra tik informacinio pobūdžio. Jeigu kyla papildomų klausimų dėl mokėsčių mokėjimo rekomenduojame kreiptis pas mokėsčių apskaitos profesionalus.</p>
               </div>
 
               <div className="col text-right">
                 <p>Pastebėjote klaidą ar turite minčių kaip pagerinti skaičiuoklę?</p>
                 <p> Rašykite <span className="badge badge-info">info@paskaiciuok.lt</span></p>
-                <p>Pagaminta MB "Loginės Operacijos"</p>
+                <p>Autorius Aleksandr Gulbickij</p>
                 <p>Visos teisės saugomos</p>
               </div>
             </div>
