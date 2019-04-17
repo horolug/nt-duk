b29f727c836df90941a97c52f51dafefcd68e001
Aleksandr Gulbickij <4efirr@gmail.com>
2019-04-17 11:39:35 +0300

    Button state management for new purchase card

--- src/Components/QuestionCard/newPurchase.js
+++ src/Components/QuestionCard/newPurchase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import moment from 'moment'
+import moment from 'moment';
 
 class newPurchase extends React.Component {
 
@@ -7,7 +7,13 @@ class newPurchase extends React.Component {
     super(props);
     this.state = {
       purchaseWithinOneYear: false,
-      newPrimaryDwelling: false
+      newPrimaryDwelling: false,
+      newDwelling: 'btn-light btn',
+      noNewDwelling: 'btn-light btn ml-2',
+      inOneYear: 'btn-light btn',
+      notInOneYear: 'btn-light btn ml-2',
+      newPrimary: 'btn-light btn',
+      noNewPrimary: 'btn-light btn ml-2',
     };
     this.newDwellingPurchase = this.newDwellingPurchase.bind(this);
     this.purchaseWithinYear = this.purchaseWithinYear.bind(this);
@@ -15,7 +21,7 @@ class newPurchase extends React.Component {
 
   newDwellingPurchase(e){
     e.preventDefault();
-    if(e.target.id === "newPrimaryDwelling"){
+    if(e.target.id === "newDwelling"){
       this.setState({
         purchaseWithinOneYear: true,
       });
@@ -30,7 +36,7 @@ class newPurchase extends React.Component {
   purchaseWithinYear(e){
     e.preventDefault();
     let fastPurchase = false;
-    if(e.target.id === "newDwellingInOneYear"){
+    if(e.target.id === "inOneYear"){
       fastPurchase = true;
     }
     this.setState({
@@ -46,29 +52,84 @@ class newPurchase extends React.Component {
     return false;
   }
 
+  handleClick(e){
+    e.preventDefault();
+
+    // Fixme - needs refactoring
+
+    if ( e.target.id === "newDwelling" ){
+      this.newDwellingPurchase(e);
+      this.setState({
+        newDwelling: 'btn-secondary btn',
+        noNewDwelling: 'btn-light btn ml-2',
+      });
+    }
+
+    if ( e.target.id === "noNewDwelling"){
+      this.newDwellingPurchase(e);
+      this.setState({
+        newDwelling: 'btn-light btn',
+        noNewDwelling: 'btn-secondary btn ml-2',
+      });
+    }
+
+    if (  e.target.id === "inOneYear" ){
+      this.purchaseWithinYear(e);
+      this.setState({
+        inOneYear: 'btn-secondary btn',
+        notInOneYear: 'btn-light btn ml-2',
+      });
+    }
+
+    if (  e.target.id === "notInOneYear" ){
+      this.purchaseWithinYear(e);
+      this.setState({
+        inOneYear: 'btn-light btn',
+        notInOneYear: 'btn-secondary btn ml-2',
+      });
+    }
+
+    if (  e.target.id === "newPrimary" ){
+      this.props.handleLastQuestion(e)
+      this.setState({
+        newPrimary: 'btn-secondary btn',
+        noNewPrimary: 'btn-light btn ml-2'
+      });
+    }
+
+    if (  e.target.id === "noNewPrimary" ){
+      this.setState({
+        newPrimary: 'btn-light btn',
+        noNewPrimary: 'btn-secondary btn ml-2',
+      });
+    }
+  }
+
   render() {
     const dueYear = parseInt(this.props.sellDate.year, 10) + 1;
-    const formattedDate = moment( dueYear+"-"+this.props.sellDate.month+"-"+this.props.sellDate.day).format("YYYY-MM-DD");
+    const dateString = dueYear+"-"+this.props.sellDate.month+"-"+this.props.sellDate.day
+    const formattedDate = moment(dateString, "YYYY-MM-DD").format("YYYY-MM-DD");
     const purchaseWithinOneYear = <div className="mt-4">
       <p>Ar naujas būstas bus perkamas iki {formattedDate}</p>
       <button
-        id="newDwellingInOneYear"
-        onClick={(e) => this.purchaseWithinYear(e)}
-        className="btn btn-light">Taip</button>
+        id="inOneYear"
+        onClick={(e) => this.handleClick(e)}
+        className={this.state.inOneYear}>Taip</button>
       <button
-        id="noNewDwellingInOneYear"
-        onClick={(e) => this.purchaseWithinYear(e)}
-        className="btn btn-light ml-2">Ne</button>
+        id="notInOneYear"
+        onClick={(e) => this.handleClick(e)}
+        className={this.state.notInOneYear}>Ne</button>
     </div>;
     const newPrimaryDwelling =  <div className="mt-4">
       <p>Ar naujai perkamame būste bus deklaruota gyvenamoji vieta iki {formattedDate}</p>
       <button
-        id="primaryDwellingInOneYear"
-        onClick={(e)=>this.props.handleLastQuestion(e)}
-        className="btn btn-light ">Taip</button>
+        id="newPrimary"
+        onClick={(e)=>this.handleClick(e)}
+        className={this.state.newPrimary}>Taip</button>
       <button
-        id="noPrimaryDwellingInOneYear"
-        className="btn btn-light ml-2">Ne</button>
+        id="noNewPrimary"
+        onClick={(e)=>this.handleClick(e)}
+        className={this.state.noNewPrimary}>Ne</button>
     </div>;
 
     const cardContent = <div className="card-body">
@@ -77,13 +138,13 @@ class newPurchase extends React.Component {
       </p>
       <div className="mt-4">
         <button
-          id="newPrimaryDwelling"
-          onClick={(e) => this.newDwellingPurchase(e)}
-          className="btn btn-light">Taip</button>
+          id="newDwelling"
+          onClick={(e) => this.handleClick(e)}
+          className={this.state.newDwelling}>Taip</button>
         <button
-          id="notnewPrimaryDwelling"
-          onClick={(e) => this.newDwellingPurchase(e)}
-          className="btn btn-light ml-2">Ne</button>
+          id="noNewDwelling"
+          onClick={(e) => this.handleClick(e)}
+          className={this.state.noNewDwelling}>Ne</button>
       </div>
 
       {this.state.purchaseWithinOneYear ?  purchaseWithinOneYear : ""}
