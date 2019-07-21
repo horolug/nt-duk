262ce47080505e513b314d8aca56bc49e5931dd2
horolug <4efirr@gmail.com>
2019-07-21 16:44:57 +0300

    Updates

---
+++ precache-manifest.c96f15042cdfe976c4a703ca68412e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8dcca4ab778345268816f21151a279",
+    "url": "/nt-duk/index.html"
+  },
+  {
+    "revision": "cf66e71c60a79e3443fb",
+    "url": "/nt-duk/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "20f5e6bb1f62529f7e34",
+    "url": "/nt-duk/static/css/main.ec84012d.chunk.css"
+  },
+  {
+    "revision": "cf66e71c60a79e3443fb",
+    "url": "/nt-duk/static/js/2.0db6cba8.chunk.js"
+  },
+  {
+    "revision": "20f5e6bb1f62529f7e34",
+    "url": "/nt-duk/static/js/main.6d537a26.chunk.js"
+  },
+  {
+    "revision": "e54ce31ddf1f25a17dad",
+    "url": "/nt-duk/static/js/runtime~main.f5fcdfd8.js"
+  },
+  {
+    "revision": "fd01ea781dabca28184f6e32f1189258",
+    "url": "/nt-duk/static/media/decision.fd01ea78.svg"
+  }
+]);
